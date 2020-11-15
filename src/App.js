10e019e766f51aebd7c0bb79407a65a9cc92bcac
Nick <github email address>
2020-11-15 12:26:40 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick()}>
+        <button onClick={() => this.handleOnClick()}>
           Click
         </button>
         <p>{this.props.items.length}</p>
@@ -22,4 +22,9 @@ class App extends Component {
   }
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return {items: state.items}
+}
+
+
+export default connect(mapStateToProps)(App);
